e47acf15fd2d7db84b041126c20c995cd21302a1
Tillwege <tillwege@gmail.com>
2022-05-09 20:43:07 +0200

    Bewegen des Korbs mit Tasten ermöglicht

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+let korb;
+
 $(() => {
-    console.log("js loaded")
+    setupGame()
 })
+
+// Funktion welche den gamestate initialisiert
+function setupGame()
+{
+    korb = $(".korb")
+    $(document).on('keydown', move)
+}
+
+
+// Funktion welche den Korb bewegt
+function move(event)
+{
+    switch(event.key)
+    {
+        case "q":
+            korb.css('left','200px');
+            korb.css('top','250px');
+            break;
+        case "y":
+            korb.css('left','200px');
+            korb.css('top','375px');
+            break;
+        case "*":
+            korb.css('left','470px');
+            korb.css('top','250px');
+            break;
+        case "-":
+            korb.css('left','470px');
+            korb.css('top','375px');
+            break;
+    }
+}
